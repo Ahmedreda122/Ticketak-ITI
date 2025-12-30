5739cbf654855b26e1c27d1c47a7f9c9acb54c57
Karim Shebl <karimshebl003@gmail.com>
2025-12-30 19:03:51 +0200

    Added validations to event creation&modification for the admin view

--- Project/Event.cpp
+++ Project/Event.cpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -38,9 +39,9 @@ private:
 public:
     Event() = default;
 
-    Event(int id,string name,Category category,Date date,
-        TicketTypePriceQuantity vipTickets,TicketTypePriceQuantity economicTickets,TicketTypePriceQuantity regularTickets)
-    {
+    Event(int id, string name, Category category, Date date,
+          TicketTypePriceQuantity vipTickets, TicketTypePriceQuantity economicTickets,
+          TicketTypePriceQuantity regularTickets) {
         this->id = id;
         this->name = name;
         this->category = category;
@@ -52,15 +53,80 @@ public:
         availableTickets = capacity;
     }
 
-    const TicketTypePriceQuantity& getVipTickets() const {
+    int isValid(string &error) {
+        int errC = 0;
+        if (name.empty()) {
+            error = "Event name cannot be empty";
+            errC++;
+        }
+
+        if (date.day < 1 || date.day > 31) {
+            if (!error.empty()) error += '\n';
+            error += "Invalid day";
+            errC++;
+        }
+
+        if (date.month < 1 || date.month > 12) {
+            if (!error.empty()) error += '\n';
+            error += "Invalid month";
+            errC++;
+        }
+
+        if (date.year < 2025) {
+            if (!error.empty()) error += '\n';
+            error += "Invalid year";
+            errC++;
+        }
+
+        if (vipTickets.quantity < 0) {
+            if (!error.empty()) error += '\n';
+            error += "VIP tickets quantity must be greater than or equal to zero";
+            errC++;
+        }
+
+        if (vipTickets.quantity > 0 && vipTickets.price <= 0) {
+            if (!error.empty()) error += '\n';
+            error += "VIP tickets price must be greater than zero";
+            errC++;
+        }
+
+        if (regularTickets.quantity < 0) {
+            if (!error.empty()) error += '\n';
+            error += "Regular tickets quantity must be greater than or equal to zero";
+            errC++;
+
+        }
+
+        if (regularTickets.quantity > 0 && regularTickets.price <= 0) {
+            if (!error.empty()) error += '\n';
+            error += "Regular tickets price must be greater than zero";
+            errC++;
+        }
+
+        if (economicTickets.quantity < 0) {
+            if (!error.empty()) error += '\n';
+            error += " Economic tickets quantity must be greater than or equal to zero";
+            errC++;
+
+        }
+        if (economicTickets.quantity > 0 && economicTickets.price <= 0) {
+            if (!error.empty()) error += '\n';
+            error += "Economic tickets price must be greater than zero";
+            errC++;
+        }
+
+        return errC;
+    }
+
+    const TicketTypePriceQuantity &getVipTickets() const {
         return vipTickets;
     }
 
-    const TicketTypePriceQuantity& getEconomicTickets() const {
+    const TicketTypePriceQuantity &getEconomicTickets() const {
         return economicTickets;
     }
 
-    const TicketTypePriceQuantity& getRegularTickets() const {
+    const TicketTypePriceQuantity &getRegularTickets() const {
         return regularTickets;
     }
 
@@ -151,8 +217,8 @@ public:
 
         // keep totals consistent
         capacity = vipTickets.quantity +
-                regularTickets.quantity +
-                economicTickets.quantity;
+                   regularTickets.quantity +
+                   economicTickets.quantity;
 
         availableTickets = capacity;
     }
@@ -161,7 +227,7 @@ public:
         this->id = id;
     }
 
-    void setName(const string& name) {
+    void setName(const string &name) {
         this->name = name;
     }
 
@@ -169,43 +235,44 @@ public:
         this->category = category;
     }
 
-    string dateToString(Date date) const{
+    string dateToString(Date date) const {
         string d = (date.day < 10 ? "0" : "") + to_string(date.day);
         string m = (date.month < 10 ? "0" : "") + to_string(date.month);
         return d + "-" + m + "-" + to_string(date.year);
     }
 
-    string categoryToString(Category category) const{
-        switch(category) {
-            case Category::Sports: return "Sports";
-            case Category::Parties: return "Parties";
-            case Category::Carnivals: return "Carnivals";
-            default: return "Unknown";
+    string categoryToString(Category category) const {
+        switch (category) {
+            case Category::Sports:
+                return "Sports";
+            case Category::Parties:
+                return "Parties";
+            case Category::Carnivals:
+                return "Carnivals";
+            default:
+                return "Unknown";
         }
     }
+
     // Logic to link fan to ticket
     // Note if returned ticket has id="0" , then booking operation is failed
-    Ticket bookEvent(int fanId, TicketTypePrice typePrice)
-    {
+    Ticket bookEvent(int fanId, TicketTypePrice typePrice) {
         Ticket createdTicket;
         switch (typePrice.type) {
             case TicketType::VIP:
-                if(vipTickets.quantity <= 0)
-                {
+                if (vipTickets.quantity <= 0) {
                     return Ticket();
                 }
                 vipTickets.quantity--;
                 break;
             case TicketType::Economic:
-                if(economicTickets.quantity <= 0)
-                {
+                if (economicTickets.quantity <= 0) {
                     return Ticket();
                 }
                 economicTickets.quantity--;
                 break;
             case TicketType::Regular:
-                if(regularTickets.quantity <= 0)
-                {
+                if (regularTickets.quantity <= 0) {
                     return Ticket();
                 }
                 regularTickets.quantity--;
@@ -216,7 +283,7 @@ public:
         }
         availableTickets--;
 
-        createdTicket.setId(to_string(tickets.size()+1));
+        createdTicket.setId(to_string(tickets.size() + 1));
         createdTicket.setFanId(fanId);
         createdTicket.setEventId(id);
         createdTicket.setTicketTypePrice(typePrice);
@@ -226,18 +293,20 @@ public:
         return createdTicket;
     }
 
-    string viewDetailsBreifly()
-    {
-        return "Name: "+name+" , Category: "+categoryToString(category)+" , Date: "+dateToString(date);
+    string viewDetailsBreifly() {
+        return "Name: " + name + " , Category: " + categoryToString(category) + " , Date: " + dateToString(date);
     }
 
-    string viewDetails() const
-    {
+    string viewDetails() const {
         return "  Event #" + to_string(id) + "\n  Name: " + name + "\n  Category: " + categoryToString(category) +
-        "\n  Date: " + dateToString(date) + "\n  Total Seats: " + to_string(capacity) + "\n  Available Seats: " + to_string(availableTickets)
-        + "\n  VIP Ticket Price: " + to_string(vipTickets.price) + " , VIP Available Tickets: " + to_string(vipTickets.quantity)
-        + "\n  Regular Ticket Price: " + to_string(regularTickets.price) + " , Regular Available Tickets: " + to_string(regularTickets.quantity)
-        + "\n  Economic Ticket Price: " + to_string(economicTickets.price) + " , Economic Available Tickets: " + to_string(economicTickets.quantity);
+               "\n  Date: " + dateToString(date) + "\n  Total Seats: " + to_string(capacity) + "\n  Available Seats: " +
+               to_string(availableTickets)
+               + "\n  VIP Ticket Price: " + to_string(vipTickets.price) + " , VIP Available Tickets: " +
+               to_string(vipTickets.quantity)
+               + "\n  Regular Ticket Price: " + to_string(regularTickets.price) + " , Regular Available Tickets: " +
+               to_string(regularTickets.quantity)
+               + "\n  Economic Ticket Price: " + to_string(economicTickets.price) + " , Economic Available Tickets: " +
+               to_string(economicTickets.quantity);
         /*
         cout<<"Event #"<<id<<endl;
         cout<<"Name: "<<name<<endl;
@@ -253,10 +322,8 @@ public:
 
     string getEventStatus();
 
-    void expireTickets()
-    {
-        for(int i=0;i<tickets.size();i++)
-        {
+    void expireTickets() {
+        for (int i = 0; i < tickets.size(); i++) {
             tickets[i].changeStatus(TicketStatus::Expired);
         }
     }
